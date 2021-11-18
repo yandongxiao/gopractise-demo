726cf053ffe79acff9b0abdc8ca8e626ef935e0b
yandongxiao <dxyan06@gmail.com>
2021-11-18 12:34:01 +0800

    feat: dsa

--- gomock/spider/spider.go
+++
@@ -1,7 +0,0 @@
-package spider
-
-//go:generate mockgen -destination mock/mock_spider.go -package spider github.com/marmotedu/gopractise-demo/gomock/spider Spider
-
-type Spider interface {
-	GetBody() string
-}
